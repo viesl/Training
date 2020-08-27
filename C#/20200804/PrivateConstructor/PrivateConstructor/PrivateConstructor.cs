51fc281d98287c6a3f98f60217b1b7e08d6be60b
Vieslav Lapin <Vieslav.Lapin@Euromonitor.com>
2020-08-27 16:07:07 +0300

    Adding private constructor project

---
+++ C#/20200804/PrivateConstructor/PrivateConstructor/PrivateConstructor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrivateConstructor
+{
+    class PrivateConstructor
+    {
+        private PrivateConstructor() { }
+
+        public static string LogMessage { get; } = "PrivateConstructor class log field used.\nCannot instantiate this class.\n";
+    }
+}
---
+++ C#/20200804/PrivateConstructor/PrivateConstructor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrivateConstructor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(PrivateConstructor.LogMessage);
+            Console.ReadLine();
+        }
+    }
+}
