725e1e528d7adb458a3be7c1d0f80f259442fede
Vieslav Lapin <vieslav.lapin@gmail.com>
2020-08-30 21:31:19 +0300

    added enum

--- C#/20200825/CSFeatures/CSFeatures/Program.cs
+++ C#/20200825/CSFeatures/CSFeatures/Program.cs
@@ -29,6 +29,30 @@ namespace CSFeatures
             //Expression-bodied members - check the method
             Program.OutMethod(out int b);
 
+            //Structs
+            Complex val1 = new Complex(7, 1);
+            Complex val2 = new Complex(2, 6);
+            Complex res = val1 + val2;
+            Console.WriteLine("First: {0}", val1);
+            Console.WriteLine("Second: {0}", val2);
+            Console.WriteLine("Result (Sum): {0}", res);
+
+            Season season = Season.Autumn;
+            //Enums
+            switch (season)
+            {
+                case Season.Spring:
+                    break;
+                case Season.Summer:
+                    break;
+                case Season.Autumn:
+                    break;
+                case Season.Winter:
+                    break;
+                default:
+                    break;
+            }
+
 
             Console.ReadLine();
         }
@@ -72,4 +96,31 @@ namespace CSFeatures
         //class related logic goes here
         public string getGreeting() => "I'm a Bus";
     }
+
+    public struct Complex
+    {
+        public int real;
+        public int imaginary;
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+        public static Complex operator +(Complex one, Complex two)
+        {
+            return new Complex(one.real + two.real, one.imaginary + two.imaginary);
+        }
+        public override string ToString()
+        {
+            return (String.Format("{0} + {1}i", real, imaginary));
+        }
+    }
+    
+}
+enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
 }
