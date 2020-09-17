f0a95b689768be860c1d1c247f2f944ba00effbc
Vieslav Lapin <vieslav.lapin@gmail.com>
2020-09-17 23:55:32 +0300

    added singleton

---
+++ C#/20200911/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed.");
+            }
+        }
+    }
+}
---
+++ C#/20200911/SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
