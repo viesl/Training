e3a8a3fbf37bd6631df32d2c05c959f1beeb65c6
Vieslav Lapin <vieslav.lapin@gmail.com>
2020-09-17 23:46:20 +0300

    small change

--- C#/20200811/HashsetExample/HashsetExample/Employee.cs
+++ C#/20200811/HashsetExample/HashsetExample/Employee.cs
@@ -21,5 +21,15 @@ namespace HashsetExample
             if (other == null) return false;
             return (this.Account.Equals(other.Account));
         }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 13;
+            var myStrHashCode =
+                !string.IsNullOrEmpty(this.Account) ?
+                    this.Account.GetHashCode() : 0;
+            hashCode = (hashCode * 397) ^ myStrHashCode;
+            return hashCode;
+        }
     }
 }
