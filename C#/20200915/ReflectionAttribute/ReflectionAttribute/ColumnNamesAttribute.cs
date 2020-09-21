f519a79508b888b8cf9a01ee638db01340310830
Vieslav Lapin <vieslav.lapin@gmail.com>
2020-09-22 00:42:55 +0300

    added reflection and attributes

---
+++ C#/20200915/ReflectionAttribute/ReflectionAttribute/ColumnNamesAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionAttribute
+{
+    class ColumnNamesAttribute : Attribute
+    {
+        public string[] Values { get; set; }
+    }
+}
---
+++ C#/20200915/ReflectionAttribute/ReflectionAttribute/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable("Product");
+            dt.Columns.Add("ProductId", typeof(Int32));
+            dt.Columns.Add("ProductName", typeof(string));
+
+            dt.Rows.Add(1, "Milk");
+            dt.Rows.Add(2, "Bread");
+            dt.Rows.Add(3, "Butter");
+            dt.Rows.Add(4, "Cheese");
+
+            List<Product> productDetails = ConvertDataTable<Product>(dt);
+
+            foreach (var item in productDetails)
+            {
+                Console.WriteLine($"Id: {item.Id}, Name: {item.Name}");
+            }
+
+        }
+
+        private static bool NamesMatched(PropertyInfo propertyInfo, string columnName)
+        {
+            var displayNameAttr = propertyInfo.GetCustomAttributes(false)
+                .OfType<ColumnNamesAttribute>().FirstOrDefault();
+            return displayNameAttr?.Values.Any(v => v == columnName) ?? false;
+        }
+
+        private static List<T> ConvertDataTable<T>(DataTable dt)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (PropertyInfo pro in temp.GetProperties())
+                {
+                    var isNameMatched = NamesMatched(pro, column.ColumnName);
+                    if (pro.Name == column.ColumnName || isNameMatched)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+                }
+            }
+            return obj;
+        }
+    }
+}
